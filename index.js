db33f7f03d52b36826ffdea4a9b5ef4d2cac6205
Tunisia Artope <artope@yahoo.com>
2022-02-16 16:54:49 -0500

    second commit

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ fetch('http://localhost:3000/menu')
         // console.log(menuData)
         menuData.forEach(item =>{
             createMenuItemsDiv(item)
+            
         })
         createDishSection(menuData[0])
+        createCartForm()
     })
 
 const menuItems = document.getElementById('menu-items')
@@ -28,9 +30,27 @@ const dishName = document.getElementById('dish-name')
 const dishDescription = document.getElementById('dish-description')
 const dishPrice = document.getElementById('dish-price')
 // console.log(dishImage, dishName, dishDescription, dishPrice)
+
+let currentMenuItem;
 function createDishSection(item){
+    currentMenuItem = item;
+
     dishImage.src = item.image
     dishName.textContent = item.name
     dishDescription.textContent = item.description
     dishPrice.textContent = item.price
+}
+
+let cartForm = document.getElementById('cart-form')
+let numberInCart = document.getElementById('number-in-cart')
+// console.log(numberInCart)
+function createCartForm(){
+    cartForm.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        // console.log('submit')
+        let addMenuItem = e.target['cart-amount'].value
+        currentMenuItem.number_in_bag += parseInt(addMenuItem)
+        numberInCart.textContent = currentMenuItem.number_in_bag
+        e.target.reset()
+    })
 }
